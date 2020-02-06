9b357c7731887b6f4a808244216b7f139eafa59b
Shreyas Khadapkar <55597268+khadapkarS20@users.noreply.github.com>
2020-02-06 21:55:02 +0530

    init

---
+++ whatsapp.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+person = input("Enter the name of the person: ")
+msg = input("Enter the message: ")
+count = int(input("Enter the count of msg: "))
+
+chrome_options = webdriver.ChromeOptions()
+browser = webdriver.Chrome('C:/Users/SHREYAS/Downloads/chromedriver_win32/chromedriver.exe',options=chrome_options)
+base_url = 'https://web.whatsapp.com/'
+browser.get(base_url)
+
+chathead=browser.find_element_by_xpath("//span[@title='{}']".format(person))
+# print(chathead)
+chathead.click()
+
+msgbox = browser.find_element_by_class_name('_3u328')
+
+input('Enter anything but scan first')
+for i in range(count):
+    msgbox.send_keys(msg + str(i))
+    sendbtn = browser.find_element_by_class_name('_3M-N-')
+    sendbtn.click()
