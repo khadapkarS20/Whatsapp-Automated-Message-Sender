92897c10972fc99e7db5cad11f1335dfab645c9e
KhadapkarS20 <shreyaskhadapkar@gmail.com>
2020-02-06 22:12:11 +0530

    Minor Changes

--- whatsapp.py
+++ whatsapp.py
@@ -9,13 +9,15 @@ browser = webdriver.Chrome('C:/Users/SHREYAS/Downloads/chromedriver_win32/chrome
 base_url = 'https://web.whatsapp.com/'
 browser.get(base_url)
 
+input('Enter anything but scan first')
+
 chathead=browser.find_element_by_xpath("//span[@title='{}']".format(person))
 # print(chathead)
 chathead.click()
 
 msgbox = browser.find_element_by_class_name('_3u328')
 
-input('Enter anything but scan first')
+
 for i in range(count):
     msgbox.send_keys(msg + str(i))
     sendbtn = browser.find_element_by_class_name('_3M-N-')
